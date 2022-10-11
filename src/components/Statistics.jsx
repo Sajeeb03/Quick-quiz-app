39098c8e11b5170647b986abe9fe7ef3b97ae154
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-10-11 22:05:57 +0600

    chart margin problem solved

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -5,7 +5,7 @@ import { CategoriesContext } from './Root';
 const Statistics = () => {
     const data = useContext(CategoriesContext);
     return (
-        <div className='w-full md:w-1/2 md:m-auto mt-5 md:mt-5'>
+        <div className='w-5/6 md:w-1/2 md:m-auto mt-5 md:mt-5'>
             <ResponsiveContainer width="100%" aspect={2}>
                 <BarChart width={150} height={40} data={data}>
                     <Bar dataKey="total" fill="#8884d8" />
