c815fedb4b7add2ac951ed816245bcc407ee4cd0
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-10-12 10:20:39 +0600

    radio button problem solved

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,6 +1,8 @@
 import { EyeIcon } from '@heroicons/react/24/solid';
-import React from 'react';
+import React, { useContext } from 'react';
 import Swal from 'sweetalert2';
+
+
 
 const Question = ({ questions, idx, correctAns, setCorrectAns }) => {
 
@@ -18,6 +20,8 @@ const Question = ({ questions, idx, correctAns, setCorrectAns }) => {
         Swal.fire(`Correct ans is:
         ${correctAnswer}`);
     }
+
+    console.log(idx)
     const questionR = question.replace('<p>', '')
     return (
 
@@ -25,7 +29,9 @@ const Question = ({ questions, idx, correctAns, setCorrectAns }) => {
             <h1 className='text-3xl font-bold mb-3'>Quiz {idx + 1}: {questionR.replace('</p>', '')}</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-2 mt-1 text-xl font-semibold'>
                 {
-                    options.map((option, idx) => <p key={idx} className={`bg-cyan-600 p-6 rounded-lg`} ><input type="radio" name='idx' onClick={() => handleAnswer(option)} /> {option}</p>)
+                    options.map((option, index) => <div key={index} className='bg-cyan-600 p-6 rounded-lg'>
+                        <input type="radio" value={`group-${idx}`} name={idx} onClick={() => handleAnswer(option)} /><label> {option}</label>
+                    </div >)
                 }
             </div>
             <EyeIcon onClick={handleCorrectAns} className='h-6 w-6 absolute top-1 right-3 mt-5'></EyeIcon>
